7391d9ffe7a32312afe49c2c19fdf76c67dd5d84
sean <seathony@gmail.com>
2015-09-17 19:26:54 -0400

    three way binding

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
-  angular.module('simple-todos-angular',['angular-meteor']);
+  angular.module('simple-todos-angular', ['angular-meteor']);
+
+  angular.module('simple-todos-angular').controller('PartiesListCtrl', ['$scope', '$meteor',
+    function ($scope, $meteor) {
+
+    $scope.parties = $meteor.collection(Parties);
+
+  }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+    }
+  });
 }
