675fb6d3defb3790206405a669ff49f75d322e1f
sean <seathony@gmail.com>
2015-09-19 09:38:10 -0400

    remove tasks that are checked only

--- app.js
+++ app.js
@@ -35,6 +35,32 @@ if (Meteor.isClient) {
     $scope.removeAll= function(){
       $scope.tasks.remove();
     };
+
+    $scope.removeAll= function(){
+      $scope.tasks.remove();
+    };
+
+    $scope.chckedIndexs=[];
+
+    $scope.checkedIndex = function (task) {
+         if ($scope.chckedIndexs.indexOf(task) === -1) {
+             $scope.chckedIndexs.push(task);
+         }
+         else {
+             $scope.chckedIndexs.splice($scope.chckedIndexs.indexOf(task), 1);
+         }
+     }
+
+     $scope.selectedTasks = function () {
+         return $filter('filter')($scope.tasks, { checked: true });
+     };
+      $scope.removeChecked=function(index){
+          angular.forEach($scope.chckedIndexs, function (value, index) {
+              var index = $scope.tasks.indexOf(value);
+              $scope.tasks.splice($scope.tasks.indexOf(value), 1);
+          });
+            $scope.chckedIndexs = [];
+     };
   }]);
 
   angular.module("simple-todos-angular").controller("TaskDetailsCtrl", ['$scope', '$stateParams',
