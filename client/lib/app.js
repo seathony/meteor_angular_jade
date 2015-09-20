af5cd46b27939864195c8f2147ab72a81241b3b8
sean <seathony@gmail.com>
2015-09-19 21:47:47 -0400

    material design

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,1 +1,17 @@
-angular.module('simple-todos-angular', ['angular-meteor', 'ui.router']);
+angular.module('simple-todos-angular', ['angular-meteor', 'ui.router', 'ngMaterial']);
+
+var themeIcons = ['$mdIconProvider' , function ($mdIconProvider) {
+
+  $mdIconProvider
+    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
+
+}];
+
+angular.module('simple-todos-angular')
+  .config(themeIcons);
